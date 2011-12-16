54e641dc4df5271ad2503dd60a22e6c54f485985
icexelloss <li_jin@brown.edu>
2011-12-15 22:22:25 -0500

    updates

--- Client/src/utilities/shortestpath/SPGraphInputGenerator.java
+++ Client/src/utilities/shortestpath/SPGraphInputGenerator.java
@@ -31,8 +31,6 @@ import utilities.filesystem.Directory;
 
 import graphs.programs.GraphEdge;
 import graphs.programs.GraphVertex;
-import graphs.programs.shortestpath.SPGraphVertex;
-import graphs.programs.shortestpath.SPGraphEdge;
 
 public class SPGraphInputGenerator extends GraphInputGenerator<GraphVertex, GraphEdge> {
 	
@@ -57,10 +55,10 @@ public class SPGraphInputGenerator extends GraphInputGenerator<GraphVertex, Grap
 
 		Random random = new Random();
 
-		Map<Integer,SPGraphVertex> vertexMap = new HashMap<Integer,SPGraphVertex>();
+		Map<Integer,GraphVertex> vertexMap = new HashMap<Integer,GraphVertex>();
 		
 		for(int i = 0; i < numberVertices; i++) {
-			SPGraphVertex vertex = new SPGraphVertex();
+			GraphVertex vertex = new GraphVertex();
 
 			graph.addVertex(vertex);
 
@@ -89,27 +87,6 @@ public class SPGraphInputGenerator extends GraphInputGenerator<GraphVertex, Grap
 			if(i % 1000 == 0)
 				System.out.printf("processed %d vertex.\n", i);
 		}		
-	}
-
-	protected void obtainSimpleGraph() {
-		graph = new DefaultDirectedGraph<GraphVertex,GraphEdge>(GraphEdge.class);
-
-		Map<Integer,SPGraphVertex> vertexMap = new HashMap<Integer,SPGraphVertex>();
-
-		for(int i = 0; i < 5; i++) {
-			SPGraphVertex vertex = new SPGraphVertex();
-
-			graph.addVertex(vertex);
-
-			vertexMap.put(i, vertex);
-		}
-
-		graph.addEdge(vertexMap.get(0), vertexMap.get(1), new SPGraphEdge(1));
-		graph.addEdge(vertexMap.get(1), vertexMap.get(2), new SPGraphEdge(1));
-		graph.addEdge(vertexMap.get(2), vertexMap.get(3), new SPGraphEdge(5));
-		graph.addEdge(vertexMap.get(3), vertexMap.get(4), new SPGraphEdge(1));
-		graph.addEdge(vertexMap.get(0), vertexMap.get(2), new SPGraphEdge(10));
-		graph.addEdge(vertexMap.get(0), vertexMap.get(3), new SPGraphEdge(6));
 	}
 
 	public static void main(String[] arguments) {
